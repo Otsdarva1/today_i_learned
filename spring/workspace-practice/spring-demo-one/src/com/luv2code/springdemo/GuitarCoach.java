00a4c7a0f9412ff057ccbc0c46d8fcade35b59cf
山崎佳裕 <yamazaki192748@gmail.com>
2019-09-02 23:41:59 +0900

    practice springframework

---
+++ spring/workspace-practice/spring-demo-one/src/com/luv2code/springdemo/GuitarCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class GuitarCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "spent 60 minutes on tapping practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

853d2c0c0f0d813eb6cf7383f750fc9972154f95
山崎佳裕 <yamazaki192748@gmail.com>
2019-09-03 23:34:46 +0900

    today i learned about setter injection and bean scope and bean life cycle.

--- spring/workspace-practice/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring/workspace-practice/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -6,7 +6,7 @@ public class TrackCoach implements Coach {
 
 	public TrackCoach() {
 	}
-	
+
 	public TrackCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
@@ -21,4 +21,13 @@ public class TrackCoach implements Coach {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
 
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+	}
 }
